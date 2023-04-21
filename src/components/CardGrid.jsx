82dd32cf6cf3f7e5ae115504517d95955a8a20dc
Allen Walker <maaakooo@gmx.de>
2023-04-21 11:43:32 +0200

    2 new methods to remove double code

--- src/components/CardGrid.jsx
+++ src/components/CardGrid.jsx
@@ -57,82 +57,64 @@ const CardGrid = () => {
 
   const removeColorFilter = (color) => {
     setFilterByColor((prevFilter) => {
-      console.log(prevFilter.filter((el) => el !== color));
       return prevFilter.filter((el) => el !== color);
     });
   };
 
-  const log = (string) => {
-    console.log(string);
-  };
-
   const moveLeft = (id) => {
     let thisCard = cards.filter((card) => card.id === id)[0];
-    let previousCard = cards
-      //filter by rarity
-      .filter((card) => isRarity(card.rarity))
-      //filter by color
-      .filter((card) => isColor(card.color))
-      //find card in rest array
-      .filter(
-        (card, index, filteredCards) =>
-          index === filteredCards.indexOf(thisCard) - 1
-      )[0];
+    let previousCard = applyFilterToCardList().filter(
+      //get the previous card
+      (card, index, filteredCards) =>
+        index === filteredCards.indexOf(thisCard) - 1
+    )[0];
     let previousCardIndex = cards.indexOf(previousCard);
-    if (previousCardIndex >= 0) {
-      let newCards = cards.filter((card) => card.id !== thisCard.id);
-      newCards.splice(previousCardIndex, 0, thisCard);
-      setCards(newCards);
-    }
+    updateCards(previousCardIndex, thisCard);
   };
 
   const moveRight = (id) => {
     let thisCard = cards.filter((card) => card.id === id)[0];
-    let nextCard = cards
-      //filter by rarity
-      .filter((card) => isRarity(card.rarity))
-      //filter by color
-      .filter((card) => isColor(card.color))
+    let nextCard = applyFilterToCardList()
       //find card in rest array
       .filter(
         (card, index, filteredCards) =>
           index === filteredCards.indexOf(thisCard) + 1
       )[0];
     let nextCardIndex = cards.indexOf(nextCard);
-    if (nextCardIndex >= 0) {
-      let newCards = cards.filter((card) => card.id !== thisCard.id);
-      newCards.splice(nextCardIndex, 0, thisCard);
-      setCards(newCards);
-    }
+    updateCards(nextCardIndex, thisCard);
   };
 
   const moveToFirst = (id) => {
     let thisCard = cards.filter((card) => card.id === id)[0];
-    let firstCard = cards
-      //filter by rarity
-      .filter((card) => isRarity(card.rarity))
-      //filter by color
-      .filter((card) => isColor(card.color))[0];
-    let lastCardIndex = cards.indexOf(firstCard);
-    if (lastCardIndex >= 0) {
-      let newCards = cards.filter((card) => card.id !== thisCard.id);
-      newCards.splice(lastCardIndex, 0, thisCard);
-      setCards(newCards);
-    }
+    let firstCard = applyFilterToCardList()[0];
+    let firstCardIndex = cards.indexOf(firstCard);
+    updateCards(firstCardIndex, thisCard);
   };
 
   const moveToLast = (id) => {
     let thisCard = cards.filter((card) => card.id === id)[0];
-    let filteredCards = cards
-      //filter by rarity
-      .filter((card) => isRarity(card.rarity))
-      //filter by color
-      .filter((card) => isColor(card.color));
+    let filteredCards = applyFilterToCardList();
     let lastCard = filteredCards[filteredCards.length - 1];
     let lastCardIndex = cards.indexOf(lastCard);
-    if (lastCardIndex >= 0) {
-      let newCards = cards.filter((card) => card.id !== thisCard.id);
-      newCards.splice(lastCardIndex, 0, thisCard);
+    updateCards(lastCardIndex, thisCard);
+  };
+
+  const applyFilterToCardList = () => {
+    return (
+      cards
+        //filter by rarity
+        .filter((card) => isRarity(card.rarity))
+        //filter by color
+        .filter((card) => isColor(card.color))
+    );
+  };
+
+  const updateCards = (targetIndex, cardToBeMovedToTargetIndex) => {
+    if (targetIndex >= 0) {
+      let newCards = cards.filter(
+        (card) => card.id !== cardToBeMovedToTargetIndex.id
+      );
+      newCards.splice(targetIndex, 0, cardToBeMovedToTargetIndex);
       setCards(newCards);
     }
   };
@@ -156,6 +138,17 @@ const CardGrid = () => {
     }
   };
 
+  const transformCardData = (card) => {
+    return {
+      id: card.mtgo_id,
+      front: card.image_uris.normal,
+      back: "",
+      color: setColor(card.color_identity),
+      sides: 1,
+      rarity: card.rarity,
+    };
+  };
+
   useEffect(() => {
     if (!cards.length)
       Promise.all([
@@ -169,24 +162,8 @@ const CardGrid = () => {
         .then(([cards1, cards2, cards3]) => {
           setCards(
             cards1.data
-              .map((card) => ({
-                id: card.mtgo_id,
-                front: card.image_uris.normal,
-                back: "",
-                color: setColor(card.color_identity),
-                sides: 1,
-                rarity: card.rarity,
-              }))
-              .concat(
-                cards2.data.map((card) => ({
-                  id: card.mtgo_id,
-                  front: card.image_uris.normal,
-                  back: "",
-                  color: setColor(card.color_identity),
-                  sides: 1,
-                  rarity: card.rarity,
-                }))
-              )
+              .map((card) => transformCardData(card))
+              .concat(cards2.data.map((card) => transformCardData(card)))
               .concat(
                 cards3.data.map((card) => ({
                   id: card.mtgo_id,
@@ -203,7 +180,6 @@ const CardGrid = () => {
   }, []);
 
   useEffect(() => {
-    log("saving...");
     localStorage.setItem("momSetList", JSON.stringify(cards));
   }, [cards]);
 
